6b358ec57c7fe8533bb384f3c9994ce4febf0ed6
TheSoulounias <142552158+TheSoulounias@users.noreply.github.com>
2024-03-13 02:12:58 -0400

    Add functioning buttons. Drawing trail. Removed clear button and instead reset grid. Fix a lot of logic issues.

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-const container = document.querySelector('.container');
+const body = document.querySelector('body');
+const button = document.querySelector('#grid-button');
+const clearButton = document.querySelector('#clear-grid');
+const inputX = document.querySelector('#grid-input-x');
+const inputY = document.querySelector('#grid-input-y');
+const container = document.createElement('container');
+container.classList.add('container');
+body.appendChild(container);
 function createGrid(rows, columns) {
-  for (let i = 0; i < rows+1; i++){
+  //remove line below for stacking grids you can scroll through
+  body.removeChild(document.querySelector('.container'));
+  const container = document.createElement('div');
+  body.appendChild(container);
+  container.classList.add('container');
+
+  for (let i = 0; i < rows; i++){
     const flexContainer = document.createElement('div');
     container.appendChild(flexContainer);
     flexContainer.classList.add('flex-container');
     
-    for(let j = 0; j < columns+1; j++){
+    for(let j = 0; j < columns; j++){
       const flexItem = document.createElement('div');
       flexContainer.appendChild(flexItem);
       flexItem.classList.add('flex-item');
+      flexItem.onmouseout = () => flexItem.setAttribute('style', "background-color:blue");
     }
   }
 }
-createGrid(4,4);
+button.onclick = () => createGrid(inputX.value,inputY.value);
+clearButton.onclick = () => body.removeChild(document.querySelector('.container'));
