c9d779049045b1dcc6a75eb924c434836598f245
TheSoulounias <142552158+TheSoulounias@users.noreply.github.com>
2024-03-13 00:43:41 -0400

    Add working grid generator by fixing typos and logic

---
+++ script.js
@@ -0,0 +1,15 @@
+const body = document.querySelector('body');
+function createGrid(rows, columns) {
+  for (let i = 0; i < rows+1; i++){
+    const flexContainer = document.createElement('div');
+    body.appendChild(flexContainer);
+    flexContainer.classList.add('flex-container');
+    
+    for(let j = 0; j < columns+1; j++){
+      const flexItem = document.createElement('div');
+      flexContainer.appendChild(flexItem);
+      flexItem.classList.add('flex-item');
+    }
+  }
+}
+createGrid(4,4);
